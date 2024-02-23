7b0f4fabd2c2b227dc4a78dd07a51d5375283011
sipneat <jake.esperson@outlook.com>
2024-02-22 16:51:36 -0800

    jordan was right, and i am dumb

--- project/Pipeline Buffers/ID_EX.v
+++ project/Pipeline Buffers/ID_EX.v
@@ -13,20 +13,14 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module ID_EX (
-    input [31:0] PC_IN, RS_IN, RT_IN,
-    input [5:0] WrAdr_IN,
+    input [31:0] PC_IN, RS_IN, RT_IN, IMM_IN,
+    input regWrite_IN, memtoReg_IN, PCtoReg_IN, branchN_IN, branchZ_IN, jump_IN, jumpMem_IN, memRead_IN, memWrite_IN,
     input [3:0] ALUop_IN,
-    input [5:0] IMM_IN,
-    input [4:0] ALUCtrl_IN,
-    input megWrite_IN, memtoReg_IN, PCtoReg_IN, branchN_IN, branchZ_IN, jump_IN, jumpMem_IN, ALU_IN, memRead_IN, memWrite_IN,
     input clk,
     /////////////////////////////////////////////////////////////////
-    output reg [31:0] PC_OUT, RS_OUT, RT_OUT,
-    output reg [5:0] WrAdr_OUT,
-    output reg [3:0] ALUop_OUT,
-    output reg [5:0] IMM_OUT,
-    output reg [4:0] ALUCtrl_OUT,
+    output reg [31:0] PC_OUT, RS_OUT, RT_OUT, IMM_OUT,
     output megWrite_OUT, memtoReg_OUT, PCtoReg_OUT, branchN_OUT, branchZ_OUT, jump_OUT, jumpMem_OUT, memRead_OUT, memWrite_OUT,
+    output [3:0] ALUop_OUT
 );
 
     always @(negedge clk)
@@ -34,19 +28,16 @@ module ID_EX (
         PC_OUT = PC_IN;
         RS_OUT = RS_IN;
         RT_OUT = RT_IN;
-        WrAdr_OUT = WrAdr_IN;
-        ALUop_OUT = ALUop_IN;
         IMM_OUT = IMM_IN;
-        ALUCtrl_OUT = ALUCtrl_IN;
-        megWrite_OUT = megWrite_IN;
+        regWrite_OUT = regWrite_IN;
         memtoReg_OUT = memtoReg_IN;
         PCtoReg_OUT = PCtoReg_IN;
         branchN_OUT = branchN_IN;
         branchZ_OUT = branchZ_IN;
         jump_OUT = jump_IN;
         jumpMem_OUT = jumpMem_IN;
-        ALU_OUT = ALU_IN;
         memRead_OUT = memRead_IN;
         memWrite_OUT = memWrite_IN;
+        ALUop_OUT = ALUop_IN;
     end
 endmodule
